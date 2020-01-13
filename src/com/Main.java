119896eba1bef18e7fd21263ddde53e62b57bd90
tevee <brydiginte69@gmail.com>
2020-01-13 12:55:47 +0100

    delete method

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,8 +1,25 @@
 package com;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+
+    public void delete(String filePath) {
+        Path path = Paths.get(filePath);
+
+        try {
+            Files.delete(path);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
