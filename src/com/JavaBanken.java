2948e47ab22dbd1beb9f9fe32e8c6e997303cf0f
Sofia <sofia.fredmans@gmail.com>
2020-01-13 11:02:38 +0100

    Konto och kundklass tillagt , variabler + konstruktor tillagd

--- src/com/JavaBanken.java
+++ src/com/JavaBanken.java
@@ -22,20 +22,16 @@ public class JavaBanken {
                 break;
             }
             case 3: {
-
                 break;
             }
             case 4: {
                 break;
-
             }
             case 5: {
                 break;
-
             }
             case 6: {
                 break;
-
             }
             default:
 
---
+++ src/com/Konto.java
@@ -0,0 +1,43 @@
+package com;
+
+public class Konto {
+    private int accountNumber;
+    private int accountBalance;
+    private int debt;
+
+    public Konto(int accountNumber, int accountBalance, int debt) {
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+        this.debt = debt;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber{
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance{
+        this.accountBalance = accountBalance;
+    }
+
+    public int getDebt() {
+        return debt;
+    }
+
+    public void setDebt{
+        this.debt = debt;
+    }
+
+}
+
+
+
+
+
